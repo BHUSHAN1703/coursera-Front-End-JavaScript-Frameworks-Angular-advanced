1260b14a7ccf1e029531c849163f2499adb22fb6
Bhushan Bhamare <bhubhamare1703@gmail.com>
2021-08-29 01:36:36 +0530

    Assignmenrt 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,9 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import {PromotionService} from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
+
 
 
 @NgModule({
@@ -47,7 +50,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
    
     
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
