777be7167cc49685f268b644d8aa39a9d3133d9a
Bhushan Bhamare <bhubhamare1703@gmail.com>
2021-12-07 23:51:54 +0530

    Reactive Forms Part 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,47 +1,41 @@
-import { Component, OnInit, ViewEncapsulation ,Input} from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DishService } from '../services/dish.service';
-
+import { Component, OnInit } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { switchMap } from 'rxjs/operators';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs/operators'
 
 @Component({
-	selector: 'app-dishdetail',
-	templateUrl: './dishdetail.component.html',
-	styleUrls: ['./dishdetail.component.scss'],
-	encapsulation: ViewEncapsulation.None
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-	dish: Dish | undefined;
-	
-	constructor(private dishservice: DishService,
+
+  dish!: Dish;
+  dishIds!: string[];
+  prev!: string;
+  next: string | undefined;
+
+  constructor(private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
-	dishIds!: string[];
-  	prev: string | undefined;
-  	next!: string;
 
-	  ngOnInit() {
-		this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-		this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-		.subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
-	  }
-	
-	  setPrevNext(dishId: string) {
-		const index = this.dishIds.indexOf(dishId);
-		this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-		this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
-	  }
-	
-	  
-	goBack(): void {
-		this.location.back();
-	  }
-	
+  ngOnInit() {
+    this.dishService.getDishIds()
+      .subscribe((dishIds) => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
 
-}
+  setPrevNext(dishIds: string) {
+    const index = this.dishIds.indexOf(dishIds);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1 ) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1 ) % this.dishIds.length];
+  }
 
-function then(arg0: (dish: any) => any) {
-	throw new Error('Function not implemented.');
+  goBack(): void {
+    this.location.back();
+  }
+
 }
