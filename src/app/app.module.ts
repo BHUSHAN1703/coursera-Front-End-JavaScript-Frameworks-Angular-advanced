0234d43a44e43c2dd256659bcc7883ac26df42c2
Bhushan Bhamare <bhubhamare1703@gmail.com>
2021-12-07 13:41:29 +0530

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -63,6 +64,7 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule,
     AppRoutingModule,
     MatDialogModule,
+    MatProgressSpinnerModule,
     MatToolbarModule,
     MatFormFieldModule, 
     MatInputModule,
